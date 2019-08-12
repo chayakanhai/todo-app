b35a79ac93238a9eff8c74af575a632dfe07ff70
Chaya Kanhaigit <chayakanhai@msn.com>
2019-08-12 10:49:59 +0200

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h>About</h>
+      <p>This is the TodoList appv1.0.o It is part of a react crash course</p>
+    </React.Fragment>
+  );
+}
+
+export default About;
